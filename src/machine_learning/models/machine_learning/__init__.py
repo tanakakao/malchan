909e7c80c4f0b4a7e2ac6f110c6d02d051dc5bdf
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-07 12:38:58 +0900

    remove legacy machine_learning model package

--- src/machine_learning/models/machine_learning/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility exports for legacy ``machine_learning.models.machine_learning`` imports."""
-
-from malchan.models.models import MLModelPipeline, SingleOutputMLModelPipeline
-
-__all__ = ["MLModelPipeline", "SingleOutputMLModelPipeline"]
