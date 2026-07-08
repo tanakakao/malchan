b075085e68064e2dbc59c84c5d339905be479163
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-08 22:37:28 +0900

    add molecular fingerprint feature module

---
+++ src/malchan/features/chemistry/fingerprints.py
@@ -0,0 +1,5 @@
+"""Molecular fingerprint feature generation.
+
+RDKit, scikit-fingerprints, and related molecular descriptors should be isolated
+here so heavy chemistry dependencies are not coupled to generic preprocessing.
+"""
