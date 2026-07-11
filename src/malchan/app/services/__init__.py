be35431ab25000b9ff4e4c945bb9c0ae682f93ec
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-11 16:29:26 +0900

    Use consolidated cached XAI service

--- src/malchan/app/services/__init__.py
+++ src/malchan/app/services/__init__.py
@@ -6,12 +6,10 @@ from .comparison_service import (
 )
 from .model_service import InMemoryModelService, ModelNotFoundError
 from .xai_comparison_hook import install_xai_comparison_hooks
-from .xai_pdp_service import install_xai_pdp_service
 from .xai_service import XaiNotReadyError, install_xai_service
 
 install_comparison_service(InMemoryModelService)
 install_xai_service(InMemoryModelService)
-install_xai_pdp_service(InMemoryModelService)
 install_xai_comparison_hooks(InMemoryModelService)
 
 __all__ = [
