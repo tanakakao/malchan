962d6f40fc3cb64ebf0044d94ef602dc2c5a2fd4
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-11 16:17:43 +0900

    Install cached XAI service extensions

--- src/malchan/app/services/__init__.py
+++ src/malchan/app/services/__init__.py
@@ -5,11 +5,14 @@ from .comparison_service import (
     install_comparison_service,
 )
 from .model_service import InMemoryModelService, ModelNotFoundError
+from .xai_service import XaiNotReadyError, install_xai_service
 
 install_comparison_service(InMemoryModelService)
+install_xai_service(InMemoryModelService)
 
 __all__ = [
     "ComparisonNotFoundError",
     "InMemoryModelService",
     "ModelNotFoundError",
+    "XaiNotReadyError",
 ]
