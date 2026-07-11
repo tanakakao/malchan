d0aa8f538eb69c4776c72e75ed9e4c4f24cc32d6
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-11 13:12:10 +0900

    Install FastAPI comparison service extensions

--- src/malchan/app/services/__init__.py
+++ src/malchan/app/services/__init__.py
@@ -1,5 +1,15 @@
 """Application services used by FastAPI and future web clients."""
 
+from .comparison_service import (
+    ComparisonNotFoundError,
+    install_comparison_service,
+)
 from .model_service import InMemoryModelService, ModelNotFoundError
 
-__all__ = ["InMemoryModelService", "ModelNotFoundError"]
+install_comparison_service(InMemoryModelService)
+
+__all__ = [
+    "ComparisonNotFoundError",
+    "InMemoryModelService",
+    "ModelNotFoundError",
+]
