8433c4d0a645a8fbeed7f34f2ecd2f3b1c0c9b1d
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-11 11:19:48 +0900

    Install analysis methods on pipeline classes

--- src/malchan/pipeline/__init__.py
+++ src/malchan/pipeline/__init__.py
@@ -2,5 +2,11 @@
 
 from .single_output import SingleOutputMLModelPipeline
 from .multi_output import MLModelPipeline
+from .analysis_extensions import install_analysis_extensions
+
+install_analysis_extensions(
+    SingleOutputMLModelPipeline,
+    MLModelPipeline,
+)
 
 __all__ = ["SingleOutputMLModelPipeline", "MLModelPipeline"]
