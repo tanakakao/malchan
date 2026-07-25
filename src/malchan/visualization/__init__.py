cc47e5fae41d0bc1c8cc0f6a08228470f96c6460
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-26 01:45:42 +0900

    Export FastAPI XAI plot helpers

--- src/malchan/visualization/__init__.py
+++ src/malchan/visualization/__init__.py
@@ -1,8 +1,35 @@
-from .machine_learning_plots import show_importances, yy_plot_ml, show_pd_and_ice, show_shap_scatter, show_shap_beeswarm, show_pd_2d
-from .inverse_analysis_plots import show_ia_result_with_pd, show_ia_result_with_pd_2d, show_ia_importance, show_ia_result_pareto
+from .inverse_analysis_plots import (
+    show_ia_importance,
+    show_ia_result_pareto,
+    show_ia_result_with_pd,
+    show_ia_result_with_pd_2d,
+)
+from .machine_learning_plots import (
+    show_importances,
+    show_pd_2d,
+    show_pd_and_ice,
+    show_shap_beeswarm,
+    show_shap_scatter,
+    yy_plot_ml,
+)
+from .xai_api_plots import (
+    show_xai_importance,
+    show_xai_pd_and_ice,
+    show_xai_shap_scatter,
+)
 
 __all__ = [
-    "show_importances", "yy_plot_ml", "show_pd_and_ice", "show_pd_2d",
-    "show_shap_scatter", "show_shap_beeswarm",
-    "show_ia_result_with_pd", "show_ia_result_with_pd_2d", "show_ia_importance", "show_ia_result_pareto",
+    "show_ia_importance",
+    "show_ia_result_pareto",
+    "show_ia_result_with_pd",
+    "show_ia_result_with_pd_2d",
+    "show_importances",
+    "show_pd_2d",
+    "show_pd_and_ice",
+    "show_shap_beeswarm",
+    "show_shap_scatter",
+    "show_xai_importance",
+    "show_xai_pd_and_ice",
+    "show_xai_shap_scatter",
+    "yy_plot_ml",
 ]
