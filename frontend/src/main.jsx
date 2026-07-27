b2c2eeca0e038fb12c16252783436fc010581f6e
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-27 11:54:14 +0900

    Load bochan-aligned web styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./bochan-theme.css";
+import "./prepare.css";
 import "./xai.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
