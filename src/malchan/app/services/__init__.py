999b664e5a448fab7337bab678c67ed1d58e6128
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-27 13:20:30 +0900

    モデル設定サービスを登録

--- src/malchan/app/services/__init__.py
+++ src/malchan/app/services/__init__.py
@@ -4,12 +4,14 @@ from .comparison_service import (
     ComparisonNotFoundError,
     install_comparison_service,
 )
+from .model_configuration_service import install_model_configuration_service
 from .model_service import InMemoryModelService, ModelNotFoundError
 from .xai_comparison_hook import install_xai_comparison_hooks
 from .xai_service import XaiNotReadyError, install_xai_service
 from .xai_shap_service import install_xai_shap_service
 
 install_comparison_service(InMemoryModelService)
+install_model_configuration_service(InMemoryModelService)
 install_xai_service(InMemoryModelService)
 install_xai_shap_service(InMemoryModelService)
 install_xai_comparison_hooks(InMemoryModelService)
