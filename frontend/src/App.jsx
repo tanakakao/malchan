a1061a870e2233d06cf0b969fe6b9f8977133158
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-30 08:45:12 +0900

    モデルファイル操作カードをアプリへ追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import ComparisonTuningControl from "./components/ComparisonTuningControl";
 import EnsembleModelSettingsControl from "./components/EnsembleModelSettingsControl";
 import MaterialDescriptorSettingsControl from "./components/MaterialDescriptorSettingsControl";
 import MaterialFeatureKindControl from "./components/MaterialFeatureKindControl";
+import ModelBundleControl from "./components/ModelBundleControl";
 import ModelResultVisualizationControl from "./components/ModelResultVisualizationControl";
 import ModelSettingDefaultsControl from "./components/ModelSettingDefaultsControl";
 import YyDiagnosticsControl from "./components/YyDiagnosticsControl";
@@ -154,6 +155,7 @@ function WorkbenchLayout() {
       <MaterialFeatureKindControl />
       <MaterialDescriptorSettingsControl />
       <ModelSettingDefaultsControl />
+      <ModelBundleControl />
       <ModelResultVisualizationControl />
       <YyDiagnosticsControl />
       <OptimizeCategoryCandidatesControl />
