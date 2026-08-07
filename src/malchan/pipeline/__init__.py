912322e52d838879ceb7f1f9f707b32e08de3691
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-08-07 20:37:55 +0900

    Install compositional XAI extension

--- src/malchan/pipeline/__init__.py
+++ src/malchan/pipeline/__init__.py
@@ -4,10 +4,14 @@ from .single_output import SingleOutputMLModelPipeline
 from .multi_output import MLModelPipeline
 from .analysis_extensions import install_analysis_extensions
 from .compositional_extensions import install_compositional_extensions
+from .compositional_xai_extensions import install_compositional_xai_extensions
 
 install_compositional_extensions(
     SingleOutputMLModelPipeline,
     MLModelPipeline,
+)
+install_compositional_xai_extensions(
+    SingleOutputMLModelPipeline,
 )
 install_analysis_extensions(
     SingleOutputMLModelPipeline,
